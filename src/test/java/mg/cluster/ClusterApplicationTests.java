17b166e2e506ea532dea9026ed8c8c11aa55f6ea
sarobidyRAKOT <rakotoarisoasarobidy630@email.com>
2024-12-15 13:01:06 +0300

    1

---
+++ src/test/java/mg/cluster/ClusterApplicationTests.java
@@ -0,0 +1,13 @@
+package mg.cluster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
